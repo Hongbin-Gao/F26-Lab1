8b1a9bfa11e1c77f7eddc83aa012da3fc15df996
Hongbin-Gao <hgao51@myseneca.ca>
2026-09-18 16:49:09 +0000

    finish lab1d

--- lab1d.py
+++ lab1d.py
@@ -2,8 +2,8 @@
 # Add comments before you do anything else.
 
 #!/usr/bin/env python3
-# Author:
-# Date:
+# Author: Hongbin
+# Date: 2026/09/18
 # Purpose: Use string methods and f-string formating.
 # Usage: python3 lab1d.py
 
@@ -13,14 +13,27 @@
 # Create another variable called “age”, the value of “age” should be your age
 # The script, when executed, should print out "How are you yourname? Happy xxth birthday!" To print this output use .format() method. 
 
+name = "Hongbin Gao"
+name.upper()
+age = 21
+print("how are you {name}? Happy {age}th birthday!".format(name=name, age=age))
+
 #TO-DO 2:
 # Create a variable called "words".
 # The value of words should be "The quick brown fox jumps over the lazy dog".
 # Use indexing to return the first and 17th charecters of "words" to the user.
 
+words = "The quick brown fox jumps over the lazy dog"
+print(words[0],words[16])
+
 #TO-DO 3:
 # Use negative indexing to return the words "jumps" and "quick" from "words" to the user.
+
+dic = words.split()
+print(dic[-5], dic[-8])
 
 #TO-DO 4:
 # Use slicing to retun everything between index 2-15 to the user.
 # Print "uick brown foxs ju" from "words".
+
+print(words[-38:-21])
